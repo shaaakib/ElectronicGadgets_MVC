0ec0324f96fc8f17fccde53e4e4736491631f576
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2024-10-29 11:50:45 +0600

    Added Delete Category in Action

--- ElectronicGadgetsWeb/Controllers/CategoryController.cs
+++ ElectronicGadgetsWeb/Controllers/CategoryController.cs
@@ -40,5 +40,68 @@ namespace ElectronicGadgetsWeb.Controllers
             return View();
         }
 
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoyFormDb = _db.Categories.Find(id);
+
+            if(categoyFormDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoyFormDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoyFormDb = _db.Categories.Find(id);
+
+            if (categoyFormDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoyFormDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+        }
+
     }
 }
